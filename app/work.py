9f63a9ebfcb6ed54e7d304e70e3cf9998f75262b
Franklyn-Ukwueze <franklynifechukwu2@gmail.com>
2023-06-19 19:50:44 +0100

    completed add to cart api

--- app/work.py
+++ app/work.py
@@ -2,11 +2,6 @@ import os
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 
-# app = Flask(__name__)
-# app.config["MONGO_URI"] = "mongodb+srv://FranklynUkwueze:20062218@cluster0.3euiqnl.mongodb.net/?retryWrites=true&w=majority"
-# #mongo_uri = os.environ.get("MONGO_URI")
-# mongo = PyMongo(app)
-
 import pymongo
 mongo_uri = os.environ.get("MONGO_URI")
 myclient = pymongo.MongoClient(mongo_uri)
