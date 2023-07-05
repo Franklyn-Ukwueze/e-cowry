78e7271f8898d034ab78ccca9b2b13caa2857454
Franklyn-Ukwueze <franklynifechukwu2@gmail.com>
2023-07-05 18:31:47 +0100

    apis for adding product

--- app/api.py
+++ app/api.py
@@ -3,9 +3,17 @@ import random
 import pymongo
 from datetime import datetime
 from flask import Flask, jsonify, request
+from flask_cors import CORS
+from flask_restful import Api
 from flask_pymongo import PyMongo
+from marshmallow import Schema, fields, validate, validates, ValidationError
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+api = Api(app)
+
+CORS(app)
 
 mongo_uri = os.environ.get("MONGO_URI")
 
@@ -14,10 +22,85 @@ mydb = myclient["cowry"]
 
 carts = mydb["carts"]
 orders = mydb["carts"]
+supplier_products = mydb["supplier_products"]
 
 current_date = datetime.now()
 
-@app.route('/api/add-to-cart', methods=['POST'])
+#######################SCHEMAS#########################
+class BrandsGateway(Schema):
+    product_type = fields.Str(required=True)
+    group_sku = fields.Str(required=True)
+    variation_type = fields.Str(required=True)
+    product_sku = fields.Str(required=True)
+    brand = fields.Str(required=True)
+    name = fields.Str(required=True)
+    retail_price = fields.Int(required=True)
+    wholesale_price = fields.Int(required=True)
+    description = fields.Str(required=True)
+    main_picture = fields.Str(required=True)
+    picture_1 = fields.Str(required=False)
+    picture_2 = fields.Str(required=False)
+    picture_3 = fields.Str(required=False)
+    picture_4 = fields.Str(required=False)
+    picture_5 = fields.Str(required=False)
+    gender = fields.Str(required=True)
+    category = fields.Str(required=True)
+    subcategory = fields.Str(required=True)
+    size = fields.Str(required=True)
+    quantity = fields.Int(required=True)
+    color = fields.Str(required=True)
+    material = fields.Str(required=True)
+    product_code = fields.Str(required=False)
+    origin = fields.Str(required=True)
+    product_id = fields.Int(required=True)
+    size_slug = fields.Str(required=False)
+    weight = fields.Str(required=True)
+    location = fields.Str(required=True)
+    currency = fields.Str(required=True)
+
+class TradeEasy(Schema):
+    image = fields.Str(required=True)
+    article = fields.Str(required=True)
+    model = fields.Str(required=True)
+    size = fields.Str(required=True)
+    quantity = fields.Int(required=True)
+    wholesale_price = fields.Str(required=True)
+    retail_price = fields.Str(required=True)
+    gender = fields.Str(required=True)
+
+class XMBO(Schema):
+    image = fields.Str(required=True)
+    ref = fields.Int(required=False)
+    brand = fields.Str(required=True)
+    ean = fields.Str(required=True)
+    description = fields.Str(required=True)
+    hs = fields.Str(required=False)
+    material = fields.Str(required=False)
+    made_in = fields.Str(required=False)
+    quantity = fields.Int(required=True)
+    retail_price = fields.Str(required=True)
+    price = fields.Str(required=True)
+    article = fields.Str(required=True)
+
+class DNCWholesale(Schema):
+    store = fields.Str(required=True)
+    lot = fields.Str(required=True)
+    merch_category = fields.Str(required=True)
+    wholesale_quantity = fields.Str(required=True)
+    wholesale_price = fields.Str(required=True)
+    brand = fields.Str(required=True)
+    description = fields.Str(required=True)
+    color = fields.Str(required=True)
+    size = fields.Str(required=True)
+    quantity = fields.Str(required=True)
+    retail_price = fields.Str(required=True)
+    image = fields.Str(required=True)
+
+
+
+########################APIs###########################
+
+@app.route('/add_to_cart', methods=['POST'])
 def add_to_cart():
     try:
         # Retrieve user account information from request
@@ -73,6 +156,84 @@ def submit_order():
         'message': 'Order submitted successfully!'
     }
     return jsonify(response)
+
+@app.route('/brandsgateway/add_product', methods=['POST'])
+def add_product():
+    try:
+        data = request.get_json()
+        payload = BrandsGateway().load(data)
+
+        product_details = {"product_type" : payload["product_type"], "group_sku" : payload["group_sku"],
+                            "variation_type" : payload["variation_type"], "product_sku" : payload["product_sku"],
+                              "brand" : payload["brand"], "name" : payload["name"], "retail_price" : payload["retail_price"],
+                                "wholesale_price" : payload["wholesale_price"], "description" : payload["description"],
+                                  "main_picture" : payload["main_picture"], "picture_1" : payload["picture_1"],
+                                    "picture_2" : payload["picture_2"], "picture_3" : payload["picture_3"],
+                                      "picture_4" : payload["picture_4"], "picture_5" : payload["picture_5"], "gender" : payload["gender"],
+                                        "category" : payload["category"], "subcategory" : payload["subcategory"], "size" : payload["size"],
+                                          "quantity" : payload["quantity"], "color" : payload["color"], "material" : payload["material"],
+                                            "product_id" : payload["product_id"], "size_slug" : payload["size_slug"], "weight" : payload["weight"],
+                                              "location" : payload["location"], "currency" : payload["currency"]}
+
+        supplier_products.insert_one(product_details)
+    except Exception as e:
+        return jsonify(message=f"An exception occurred: {e}", status=False)
+    else:
+        return jsonify({'message': 'product added successfully.'}), 200
+    
+@app.route('/tradeeasy/add_product', methods=['POST'])
+def add_product():
+    try:
+        data = request.get_json()
+        payload = TradeEasy().load(data)
+
+        product_details = {"image" : payload["image"], "article" : payload["article"],
+                            "model" : payload["model"], "size" : payload["size"],
+                              "quantity" : payload["quantity"], "price" : payload["price"], "retail_price" : payload["retail_price"], "gender" : payload["gender"]}
+
+        supplier_products.insert_one(product_details)
+    except Exception as e:
+        return jsonify(message=f"An exception occurred: {e}", status=False)
+    else:
+        return jsonify({'message': 'product added successfully.'}), 200
+
+@app.route('/dncwholesale/add_product', methods=['POST'])
+def add_product():
+    try:
+        data = request.get_json()
+        payload = DNCWholesale().load(data)
+
+        product_details = {"store" : payload["store"], "lot" : payload["lot"],
+                            "merch_category" : payload["merch_category"], "wholesale_quantity" : payload["wholesale_quantity"],
+                              "color" : payload["color"], "brand" : payload["brand"], "retail_price" : payload["retail_price"],
+                                "wholesale_price" : payload["wholesale_price"], "description" : payload["description"],
+                                  "size" : payload["size"], "image" : payload["image"],
+                                    "quantity" : payload["quantity"]}
+
+        supplier_products.insert_one(product_details)
+    except Exception as e:
+        return jsonify(message=f"An exception occurred: {e}", status=False)
+    else:
+        return jsonify({'message': 'product added successfully.'}), 200
+
+@app.route('/xmbo/add_product', methods=['POST'])
+def add_product():
+    try:
+        data = request.get_json()
+        payload = XMBO().load(data)
+
+        product_details = {"image" : payload["image"], "ref" : payload["ref"],
+                            "brand" : payload["brand"], "ean" : payload["ean"],
+                              "hs" : payload["hs"], "material" : payload["material"], "made_in" : payload["made_in"],
+                                "retail_price" : payload["retail_price"], "description" : payload["description"],
+                                  "quantity" : payload["quantity"], "price" : payload["price"],
+                                    "article" : payload["article"]}
+
+        supplier_products.insert_one(product_details)
+    except Exception as e:
+        return jsonify(message=f"An exception occurred: {e}", status=False)
+    else:
+        return jsonify({'message': 'product added successfully.'}), 200
 
 if __name__ == '__main__':
     app.run(debug=False, use_reloader=False)
