515ac4dc00afb50a4386ee42ba2c3039dd9b5dd2
Franklyn-Ukwueze <franklynifechukwu2@gmail.com>
2023-09-01 02:22:37 +0100

    order tracking API

--- app/api.py
+++ app/api.py
@@ -422,6 +422,63 @@ def get_payment(user_id):
     else:
         return jsonify({"message": "Payment information not found."}), 404
 
+@app.route('/order/<string:order_id>/status', methods=['GET', 'PUT'])
+def order_status(order_id):
+    if request.method == 'GET':
+        order = orders.find_one({'order_id': order_id}, {'_id': 0})
+        if order:
+            return jsonify({
+                'message': f"Order with order id {order_id} retrieved successfully",
+                'data': order,
+                'status': True
+            })
+        else:
+            return jsonify({
+                'message': f"Could not find order with order id {order_id}",
+                'data': None,
+                'status': False
+            }), 404
+    elif request.method == 'PUT':
+        data = request.json
+        status = data.get('status')
+        stage = data.get('stage')
+        admin_email = data.get('admin-email')
+        customer_email = data.get('customer-email')
 
+        # The payload the client sent is incomplete 
+        if not status or not stage or not admin_email or not customer_email:
+            return jsonify({
+                'message': 'Missing required payload',
+                'data': None,
+                'status': False
+            }), 400
+
+        # Update the order status in the database
+        update = orders.update_one({'order_id': order_id}, {
+            '$set': {
+                'status': status,
+                'stage': stage
+            }
+        })
+        if update.matched_count == 0:
+            return jsonify({
+                'message': f"Could not find order with order id {order_id}",
+                'data': None,
+                'status': False
+            }), 404
+        
+        # Send out emails to admin and customer
+        subject = f"Update to order ({order_id}) - {status}"
+        recipients = [admin_email, customer_email]
+        body = f"The order with order id {order_id} has been updated"
+        message = Message(subject=subject, recipients=recipients, body=body)
+        mail.send(message)
+
+        return jsonify({
+            'message': f"Order with order id {order_id} has been successfully updated",
+            'data': None,
+            'status': True
+        }), 200
+        
 if __name__ == '__main__':
     app.run(debug=False, use_reloader=False)
